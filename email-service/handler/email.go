2c6e9b3330b712c70325e922ead59efb7f452521
ghoiufyia <ghoiufyia@163.com>
2019-01-10 20:28:20 +0800

    email

--- email-service/handler/email.go
+++ email-service/handler/email.go
@@ -2,17 +2,15 @@ package handler
 
 import (
 	email "WxApp.kindle/email-service/proto"
-	"net"
 	"context"
-	"log"
-	"google.golang.org/grpc"
+
 )
 
 type service struct {
 
 }
 
-func (s *service) Create(ctx context.Context,req *email.CreateRepuest) (*email.CreateResponse,error) {
-	
+func (s *service) Create(ctx context.Context,req *email.CreateRequest) (*email.CreateResponse,error) {
+
 
 }
--- email-service/models/mysql.go
+++ email-service/models/mysql.go
@@ -2,9 +2,12 @@ package models
 
 import (
 	"github.com/astaxie/beego/orm"
+    _ "github.com/go-sql-driver/mysql" // 导入数据库驱动
 	email "WxApp.kindle/email-service/proto"
 )
 
 func init()  {
+	orm.RegisterDriver("mysql", orm.DR_MySQL)
 	orm.RegisterModel(new(email.Email))
+
 }
