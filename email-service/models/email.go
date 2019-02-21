1cac53baa65d1981c403a00ac1426fbc538c8526
ghoiufyia <ghoiufyia@163.com>
2019-02-21 20:11:29 +0800

    email

--- email-service/models/email.go
+++ email-service/models/email.go
@@ -32,5 +32,14 @@ type Email struct {
 	ToEmail		string `gorm:"type:varchar(100)"`
 	FilePath	string `gorm:"type:varchar(255)"`
 }
-
+create table `user_email` (
+	`id` varchar(255) not null primary key,
+	`user_id` varchar(255) not null default '',
+	`email` varchar(100) not null default '',
+	`created_at` int(11) not null default 0,
+	`updated_at` int(11) not null default 0,
+	`deleted_at` int(11) not null default 0,
+	unique key (`user_id`),
+	unique key (`email`)
+	)engine=innodb character set utf8mb4; 
 
