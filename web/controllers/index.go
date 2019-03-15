7c73cfbe3648455267cbb80400e793b7d8acb4e5
ghoiufyia <ghoiufyia@163.com>
2019-03-15 22:38:35 +0800

    dd

--- web/controllers/index.go
+++ web/controllers/index.go
@@ -6,13 +6,16 @@ import (
 	"github.com/ghoiufyia/WxApp.kindle/web/dogo"
 	"fmt"
 	// "io"
-	// "github.com/ghoiufyia/WxApp.kindle/web/models"
-	"github.com/jinzhu/gorm"
+	// "net/http"
+	// "encoding/json"
+	// email_pb "github.com/ghoiufyia/WxApp.kindle/email-service/proto/email"
+	// "google.golang.org/grpc"
+	// "log"
+	// "context"
 )
 
 type IndexController struct {
 	dogo.Controller
-	DB *gorm.DB
 	Name	string
 	age		int32
 }
@@ -20,49 +23,36 @@ type IndexController struct {
 func (i *IndexController)Index ()  {
 	fmt.Printf("adsd===============================")
 	// fmt.Println(i.Ctx)
-	fmt.Printf("%v",i.Ctx.Request)
+	// fmt.Printf("%+v",i.Ctx.Request)
 	// io.WriteString(i.Ctx.ResponseWriter, "Hello from a HandleFunc #2!\n")
+	fmt.Printf("adsd===============================")
 
-	user_email := models.UserEmail{
-		BaseModel:models.BaseModel{
-			ID:			uuid.New(),
-			CreatedAt: 	time.Now().Unix(),
-			UpdatedAt: 	time.Now().Unix(),
-			DeletedAt: 	0,
-		},
-		UserId:user_id,
-		Email:email,
-	}
-	fmt.Println("%v",user_email)
-	if err := s.Db.Create(user_email).Error; err != nil {
-		return nil, err
-	}
+	// w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	// w.WriteHeader(200)
+	// json.NewEncoder(w).Encode(map[string]interface{}{
+	// 	"msg": "ok",
+	// })
 
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(200)
+	// // 连接远端服务
+	// conn,err := grpc.Dial(ADDRESS,grpc.WithInsecure())
+	// if err != nil {
+	// 	log.Fatalf("connect error %v",err)
+	// }
+	// defer conn.Close()
+	// // 定义客户端
+	// client := email_pb.NewEmailServiceClient(conn)
 
-	// 连接远端服务
-	conn,err := grpc.Dial(ADDRESS,grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("connect error %v",err)
-	}
-	defer conn.Close()
-	// 定义客户端
-	client := email_pb.NewEmailServiceClient(conn)
+	// var resuest = email_pb.CreateEmailRequest{UserId:"fghfyffffffffjy",Email:"ddd@11.com"}
 
-	var resuest = email_pb.CreateEmailRequest{UserId:"fghfyffffffffjy",Email:"ddd@11.com"}
+	// // 调用 RPC
+	// resp, err := client.CreateEmail(context.Background(), &resuest)
+	// if err != nil {
+	// 	log.Printf("create email error: %v", err)
+	// }
 
-	// 调用 RPC
-	resp, err := client.CreateEmail(context.Background(), &resuest)
-	if err != nil {
-		log.Printf("create email error: %v", err)
-	}
-
-	log.Printf("created: %t", resp.Msg)
-	conn.Close()
-	json.NewEncoder(w).Encode(map[string]interface{}{
-		"msg": "ok",
-	})
+	// log.Printf("created: %t", resp.Msg)
+	// conn.Close()
+	
 
 }
 
