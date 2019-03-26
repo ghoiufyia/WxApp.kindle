2886fefafe5f332e6fed76e53fe93ac7a8ae7fab
ghoiufyia <ghoiufyia@163.com>
2019-03-26 13:54:22 +0800

    user

---
+++ do-micro/model.go
@@ -0,0 +1,12 @@
+package dogo
+
+import (
+	"time"
+)
+
+type BaseModel struct {
+	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+}
