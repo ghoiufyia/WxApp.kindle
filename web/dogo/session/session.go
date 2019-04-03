e3e0b1135a256da58b08eda8fec74b3e38e865d5
ghoiufyia <ghoiufyia@163.com>
2019-04-03 20:27:32 +0800

    session

--- web/dogo/session/session.go
+++ web/dogo/session/session.go
@@ -2,20 +2,38 @@ package session
 
 import (
 	// "github.com/gorilla/sessions"
+	"sync"
 )
 
-type SessionManager struct {
+type Manager struct {
+	cookieName	string
+	lock		sync.Mutex
+	lifetime	int64
 	Handler		handler
 }
+var lock sync.Mutex
+var DefaultManager = Manager{
+	cookieName:"dogo_sid",
+	lifetime: 60*60*24,
+	Handler:	&FileSession {
+		savePath: "storage/session",
+	},
+}
 
-func NewManager() *SessionManager {
-	return &SessionManager{
+func NewManager() *Manager {
+	return &Manager{
+		cookieName: "dogo_sid",
+		lifetime: 60*60*24,
 		Handler:	&FileSession {
-			lifetime: 60*60*24,
 			savePath: "storage/session",
 		},
 	}
 }
+
+type Session interface {
+	sessionId() (string)
+}
+
 
 
 // SetSessionService(r *http.Request, w http.ResponseWriter)
